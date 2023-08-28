164e0f24b36c67365cae49e6d4586fb9b29bd23c
Sushil97 <sushil500530@gmail.com>
2023-08-28 19:23:57 +0600

    some code

--- promise.js
+++ promise.js
@@ -1,4 +1,13 @@
 const getData = new Promise((resolve,reject) =>{
-    resolve(56665) ;
+    const num = Math.random()*10 ;
+    console.log(num) ;
+    if(num <5){
+        resolve(56665) ;
+    }
+    else{
+        reject("No Data Available!")
+    }
 })
-getData.then(data => console.log(data + 5))
+getData
+    .then(data => console.log(data + 5))  
+    .catch(err => console.log('ERR', err))
